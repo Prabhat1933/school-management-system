e96253bfe74a71be1662e9ec052591ff37430d66
prabhat <prabhatchand888@gmail.com>
2025-04-17 00:47:17 +0530

    17 April Changes

---
+++ src/Page/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../component/header/header'
+import ProductCategory from "../../component/productCategory/productCategory"
+
+export default function Dashboard() {
+  return (
+    <div>
+        <Header/>
+        <ProductCategory/>
+    </div>
+  )
+}
